823a84a1186286b7d5eec777d16256ea706c4e7c
devmariaeliana <elianamotadeoliveira@gmail.com>
2023-11-19 02:43:50 -0300

    Novas atualizações

--- ponteiros.c
+++ ponteiros.c
@@ -1,21 +1,54 @@
-int main (){
+/* int main (){
+
   int num = 42;
   float pi = 3.14;
   char ch = 'A';
 
 //Criação dos ponteiros:
 
-  int *ptr_int;
-  float *ptr_float;
-  char *ptr_char;
+     int *ptr_int;
+     float *ptr_float;
+     char *ptr_char; 
 
 //Atribuição do endereço de memória da variável:
 
-  ptr_int = &num; 
-  ptr_float = &pi;
-  ptr_char = &ch;
+     ptr_int = &num; 
+     ptr_float = &pi;
+     ptr_char = &ch; 
+
+Usando os operadores de referência "&" e desreferência "*":
+
+  int *ptr;
+
+Atribuição de endereço ao ponteiro ptr:
+
+  ptr = &num;
+
+Imprimir o endereço da váriavel:
+
+  printf("Endereço de num: %p\n", &num);
+
+Imprimir o endereço armazenado no ponteiro:
+  
+  printf("Endereço armazenado em ptr: %p\n", ptr);
+
+return 0; */
+
+//Compilando o exemplo:
+
+int main (){
+
+  int num = 42;
+  int *ptr = &num;
+  
+  printf("Valor de num: %d\n", num);
+  printf("Endereço de memória de num: %p\n", &num);
+  printf("Valor apontado por ptr: %d\n", *ptr);
+  printf("Endereço de memória apontado por ptr: %p\n", ptr);
+
+  *ptr = 98;
+
+  printf("Novo valor de num: %d\n", num);
 
 return 0;
-
-
 }
